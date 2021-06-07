196779ef4dd8c064935172921dcb5e48a9733dde
Cristian Bravo <cristiangabrielbravo92@gmail.com>
2021-06-07 20:47:22 -0300

    Archivo yotube-downloader.py contiene el codigo del programa para descargar videos de youtube usando pytube3

---
+++ youtube-downloader.py
@@ -0,0 +1,10 @@
+from pytube3 import Youtube
+
+url  =    input('Ingresar URL del video que desea descargar: ')
+video_a_descargar  = YouTube(url)
+
+print("Descargando el video " + video_a_descargar.title)
+
+video_a_descargar = video_a_descargar.streams.get_highest_resolution()
+
+video_a_descargar.download()
