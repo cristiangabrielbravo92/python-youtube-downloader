ec228520d4e0d0853260519a77431564b3818cf8
CristianBravo92 <cristiangabrielbravo92@gmail.com>
2021-06-09 20:55:41 -0300

    Función para chequear qué sistema operativo está corriendo el script y a partir de ahí ver cómo abrir una carpeta agregada

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -1,10 +1,24 @@
 from pytube import YouTube
 import os
+import sys
 import subprocess
 
 '''
 Script para descargar videos de youtube en su mejor resolución y guardarlos en la carpeta donde se ejecuta el script a partir de ingresar la url del video durante la ejecución.
 '''
+
+def open_folder(path):
+	´´´
+	Esta función chequea qué sistema operativo está siendo usado para ver cómo abrir una carpeta.
+	Más info en:
+	- https://docs.python.org/3/library/sys.html	
+	´´´
+    if sys.platform.startswith('darwin'):
+        subprocess.check_call(['open', '--', path])
+    elif sys.platform.startswhith('linux'):
+        subprocess.check_call(['gnome-open', '--', path])
+    elif sys.platform.startswith('win32'):
+        subprocess.check_call(['explorer', path])
 
 video_url  =    input('Pegue aquí la URL del video que desea descargar y luego presione Enter: ')
 video_a_descargar  = YouTube(video_url)
