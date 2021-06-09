419da32a349d84a05bd7e933ac855d0ea775a2b3
Cristian Bravo <cristiangabrielbravo92@gmail.com>
2021-06-08 23:09:17 -0300

    Cambios en nombres de variables y mensajes en pantalla. Más bloque de código en comentarios.
    Revisar el bloque de código en comentarios y tratar de hacer que funcione.

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -1,12 +1,28 @@
 from pytube import YouTube
+import os
+#import subprocess
 
-video_url  =    input('Pegur aquí la URL del video que desea descargar y luego presione Enter: ')
+
+video_url  =    input('Pegue aquí la URL del video que desea descargar y luego presione Enter: ')
 
 
 video_a_descargar  = YouTube(video_url)
 
-print("Descargando el video " + video_a_descargar.title)
+nombre_video = video_a_descargar.title
+
+print("Descargando \'" + nombre_video + "\'")
 
 video_a_descargar = video_a_descargar.streams.get_highest_resolution()
 
+working_directory = os.getcwd() + "/"
+
+print("Guardando el video en " + working_directory)
+
 video_a_descargar.download()
+
+
+#Revisar este bloque para que al terminar de descargar, se abra la ventana que contiene el archivo descargado y haga focus en el archivo
+#wd_plus_file = working_directory + "readme.md"
+#subprocess.Popen(r'explorer /select,"{}"'.format(wd_plus_file))
+
+input("Programa Finalizado con éxito, presione Enter para salir")
