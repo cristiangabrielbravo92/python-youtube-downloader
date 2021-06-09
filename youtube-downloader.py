97bf70deafe131512b5991e5809d10f74fe4a5aa
CristianBravo92 <cristiangabrielbravo92@gmail.com>
2021-06-09 13:26:40 -0300

    Bug del bloque de código de abrir la carpeta y seleccionar el archivo corregido, se solucionó solo abriendo la carpeta. Cambios en el orden de los bloques de código, descripción y comentarios agregados.

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -1,28 +1,23 @@
 from pytube import YouTube
 import os
-#import subprocess
+import subprocess
 
+'''
+Script para descargar videos de youtube en su mejor resolución y guardarlos en la carpeta donde se ejecuta el script a partir de ingresar la url del video durante la ejecución.
+'''
 
 video_url  =    input('Pegue aquí la URL del video que desea descargar y luego presione Enter: ')
-
-
 video_a_descargar  = YouTube(video_url)
 
-nombre_video = video_a_descargar.title
-
+nombre_video = video_a_descargar.title #obtiene el nombre del video para mostrarlo en pantalla en la siguiente línea
 print("Descargando \'" + nombre_video + "\'")
 
-video_a_descargar = video_a_descargar.streams.get_highest_resolution()
-
-working_directory = os.getcwd() + "/"
-
+working_directory = os.getcwd() #obtiene la ubicación donde se está ejecutando el script y donde se guardará el video 
 print("Guardando el video en " + working_directory)
 
-video_a_descargar.download()
+video_a_descargar = video_a_descargar.streams.get_highest_resolution() #obtiene la mejor resolución disponible del video
+video_a_descargar.download() #y la descarga
 
-
-#Revisar este bloque para que al terminar de descargar, se abra la ventana que contiene el archivo descargado y haga focus en el archivo
-#wd_plus_file = working_directory + "readme.md"
-#subprocess.Popen(r'explorer /select,"{}"'.format(wd_plus_file))
+subprocess.Popen('explorer "{}"'.format(working_directory)) #antes de finalizar, abre en el explorador de archivos la carpeta donde se descargó
 
 input("Programa Finalizado con éxito, presione Enter para salir")
